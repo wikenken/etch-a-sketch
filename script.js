deee921557c626d4d6e358604c54ab4c25004837
wikenken <wikenken@proton.me>
2023-02-13 16:27:51 +0800

    set up a 100*100 cells within canvas

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-*{
-    margin: auto;
+const canvas = document.querySelector(".eas-canvas");
+const resizeBtn = document.querySelector("button");
+
+function setCanvas(value) {
+  //creates 16x16 divs inside container
+  for (let i = 0; i < value * value; i++) {
+    let pixel = document.createElement("div");
+    canvas.appendChild(pixel);
+    pixel.classList.add("white");
+    pixel.addEventListener("mouseover", () => {
+      pixel.classList.add("black");
+    });
+  }
 }
+
+setCanvas(100); //default size
