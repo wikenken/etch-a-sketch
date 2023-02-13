d0ba1c0e4b0085fcd70631e0e4f10c360e373462
wikenken <wikenken@proton.me>
2023-02-13 18:12:28 +0800

    dynamic canvas size

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-const canvas = document.querySelector(".eas-canvas");
-const clear = document.querySelector("button");
+const canvasBtn = document.querySelector(".eas-canvas");
+const resizeBtn = document.querySelector(".resize");
+const clearBtn = document.querySelector(".clear");
 
-for (let i = 0; i < 256; i++) {
-  let pixel = document.createElement("div");
-  canvas.appendChild(pixel);
+function setCanvas(value) {
+  for (let i = 0; i < value * value; i++) {
+    let pixel = document.createElement("div");
+    canvasBtn.appendChild(pixel);
 
-  pixel.classList.add("cellSize", "white");
-  pixel.addEventListener("mouseover", () => {
-    pixel.classList.remove("white");
-    pixel.classList.add("black");
-  });
+    pixel.style.cssText = `flex: 1 1 ${720 / value}px`;
+    pixel.classList.add("white");
+    pixel.addEventListener("mouseover", () => {
+      pixel.classList.remove("white");
+      pixel.classList.add("black");
+    });
 
-  //clear canvas
-  clear.addEventListener("click", () => {
-    pixel.className = "";
-    pixel.classList.add("cellSize", "white");
-  });
+    //clear canvas
+    clearBtn.addEventListener("click", () => {
+      pixel.className = "";
+      pixel.classList.add("cellSize", "white");
+    });
+  }
 }
+
+setCanvas(100); //default canvas size
+
+// resizeBtn.addEventListener("click", () => {
+//   value = setCanvas(prompt("Set desired canvas size"));
+//   return value;
+// });
